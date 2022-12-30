396bb91fa1fa1f5f321b2d9fc774419991c1e079
Rytis Klimavicius <rytiskli@gmail.com>
2022-12-30 21:29:11 +0200

    formatted files. added gitignore

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ var upgrCounter = document.getElementById("upgrCounter");
 
 var outText = document.getElementById("outText");
 
-var timesClicked  = 0;
+var timesClicked = 0;
 var timesUpgraded = 0;
 var add = 1;
 
@@ -13,14 +13,10 @@ var timesToClick = 50;
 button.onclick = mainClicked;
 upgrade1.onclick = upgradeClicked;
 
-function upgradeClicked(clicked)
-{
-    if (timesClicked < timesToClick)
-    {
+function upgradeClicked(clicked) {
+    if (timesClicked < timesToClick) {
         alert("You need " + (timesToClick - timesClicked) + " more click(s) to get the upgrade!");
-    }
-    else
-    {
+    } else {
         add += 1;
         timesClicked -= timesToClick;
         timesUpgraded += 1;
@@ -30,8 +26,7 @@ function upgradeClicked(clicked)
     }
 }
 
-function mainClicked(clicked)
-{
+function mainClicked(clicked) {
     timesClicked += add;
     outText.innerHTML = "Clicked: " + timesClicked;
 }        
